9f6a5b9a9ed559aa2feb6c87903875fef9f4cadf
tom <tom@10.11.33.192>
2017-08-11 21:24:30 +1000

    Added class with Metamodel keywords

---
+++ src/main/java/org/schemaspy/model/xml/MetaModelKeywords.java
@@ -0,0 +1,21 @@
+package org.schemaspy.model.xml;
+
+public class MetaModelKeywords {
+
+	public final static String REMOTE_CATALOG = "remoteCatalog";
+	public final static String REMOTE_SCHEMA = "remoteSchema";
+
+	public final static String NAME = "name";
+	public final static String COMMENTS = "comments";
+	public final static String TYPE = "type";
+	public final static String ID = "id";
+	public final static String SIZE = "size";
+	public final static String DIGITS = "digits";
+	
+	public final static String NULLABLE = "nullable";
+	public final static String AUTO_UPDATED = "autoUpdated";
+	public final static String PRIMARY_KEY = "primaryKey";
+	public final static String DEFAULT_VALUE = "defaultValue";
+	public final static String FOREIGN_KEY = "foreignKey";
+
+}
